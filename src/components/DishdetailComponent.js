15f1ea62adfe6d4231e132a3b132300a5148b225
Dangelo Arrivillaga <talgidisoluciones@gmail.com>
2021-06-27 16:37:27 -0400

    Manteniendo la immutabilidad de los estados y las props como en el ejercicio anterior que agregar un obejeto nuevo de comentario, pero una vez actualizada la ventana se borra, se instalo thuck y looger de redux para realizar cambios en la accion antes de que llegue a la tienda y poder visualizar en consola los procesos durante la transision vs 4.1.0

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -4,6 +4,7 @@ import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Card, CardImg, CardText, CardBody, CardTitle, ListGroup, ListGroupItem,
         Breadcrumb, BreadcrumbItem, Button, Row, Col, Label, 
         Modal, ModalHeader, ModalBody } from 'reactstrap';
+import { Loading } from './LoadingComponent';
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -145,7 +146,25 @@ function RenderDish({dish}) {
 }
 
 const DishDetail = (props) => {
-    if(props.dish != null)
+    if (props.isLoading) {
+        return(
+            <div className="container">
+                <div className="row">            
+                    <Loading />
+                </div>
+            </div>
+        );
+    }
+    else if (props.errMess) {
+        return(
+            <div className="container">
+                <div className="row">            
+                    <h4>{props.errMess}</h4>
+                </div>
+            </div>
+        );
+    }
+    else if (props.dish != null) 
         return(
             <div className="container">
                 <div className="row">
